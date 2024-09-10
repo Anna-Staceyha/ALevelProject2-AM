5712a844bf40ada503de6b6d7c68cd9f3341f5eb
Anna-Staceyha <23mcdonalda@centralfoundationboys.co.uk>
2024-09-10 13:00:02 +0100

    changes

--- main.py
+++ main.py
@@ -139,3 +139,81 @@ def main_menu():
 
 
 main_menu()
+
+def play_level1():
+    while True:
+        play_level1_MOUSE_POS = pygame.mouse.get_pos()
+
+        SCREEN.fill("white")
+
+        play_level1_TEXT = get_font(45).render("This is the LEVEL 1 screen.", True, "Black")
+        play_level1_RECT = play_level1_TEXT.get_rect(center=(640, 260))
+        SCREEN.blit(play_level1_TEXT, play_level1_RECT)
+
+        play_level1_BACK = Button(image=None, pos=(640, 460), 
+                            text_input="BACK", font=get_font(75), base_color="Black", hovering_color="Green")
+
+        play_level1_BACK.changeColor(play_level1_MOUSE_POS)
+        play_level1_BACK.update(SCREEN)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+           
+
+        pygame.display.update()
+
+
+
+def level_menu():
+    while True:
+        SCREEN.blit(BG, (0, 0))
+
+        LEVELS_MOUSE_POS = pygame.mouse.get_pos()
+
+        LEVELS_TEXT = get_font(80).render("Levels", True, "#89cff0")
+        LEVELS_RECT = LEVELS_TEXT.get_rect(center=(640, 100))
+
+        LEVELS_BACK = Button(image=None, pos=(640, 660), 
+                            text_input="BACK", font=get_font(75), base_color="White", hovering_color="Green")
+
+        LEVELS_BACK.changeColor(LEVELS_MOUSE_POS)
+        LEVELS_BACK.update(SCREEN)
+
+        LEVELS_LEVEL1 = Button(image=pygame.image.load("assets/Play Rect.png"), pos=(640, 250), 
+                            text_input="Level 1", font=get_font(55), base_color="#d7fcd4", hovering_color="White")
+        
+        LEVELS_LEVEL1.changeColor(LEVELS_MOUSE_POS)
+        LEVELS_LEVEL1.update(SCREEN)
+    
+
+        LEVELS_LEVEL2 = Button(image=pygame.image.load("assets/Play Rect.png"), pos=(640, 400), 
+                            text_input="Level 2", font=get_font(55), base_color="#d7fcd4", hovering_color="White")
+        
+        LEVELS_LEVEL2.changeColor(LEVELS_MOUSE_POS)
+        LEVELS_LEVEL2.update(SCREEN)
+
+
+        LEVELS_LEVEL3 = Button(image=pygame.image.load("assets/Play Rect.png"), pos=(640, 550), 
+                            text_input="Level 3", font=get_font(55), base_color="#d7fcd4", hovering_color="White")
+        
+        LEVELS_LEVEL3.changeColor(LEVELS_MOUSE_POS)
+        LEVELS_LEVEL3.update(SCREEN)
+
+        SCREEN.blit(LEVELS_TEXT, LEVELS_RECT)
+
+        for button in [LEVELS_LEVEL1, LEVELS_LEVEL2, LEVELS_LEVEL3]:
+            button.changeColor(LEVELS_MOUSE_POS)
+            button.update(SCREEN)
+        
+        for event in pygame.event.get():      
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if LEVELS_LEVEL1.checkForInput(LEVELS_MOUSE_POS):
+                    play_level1()
+                if LEVELS_LEVEL2.checkForInput(LEVELS_MOUSE_POS):
+                    play_level2()
+                if LEVELS_LEVEL3.checkForInput(LEVELS_MOUSE_POS):
+                    play_level3()
+
+        pygame.display.update()
